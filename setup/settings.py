929b12e03fdf84d451574dc25506f7f6f9518039
ingridalvesz <ingrid.roberta.alves.s@outlook.com>
2023-11-22 19:54:42 -0300

    #accessing data

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria.apps', 'GaleriaConfig',
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
